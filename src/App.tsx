a91449bcaa6a2d113ef8f94e3ad839ec2a1ba612
InnocentEdem <innocent.kuwornu@amalitech.org>
2022-02-08 09:14:38 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,21 @@ import BooleanInput from './components/BooleanInput';
 import LargeTextInput from './components/LargeTextInput';
 import moment from 'moment';
 import DateInput from './components/DateInput';
-import { MediumTableHeader, LargeTableHeader, SmallTableHeader } from './components/TableHeaders';
+import { MediumTableHeader } from './components/TableHeaders';
 import { LargeContent, SmallContent } from './components/TableContent';
 import { nanoid } from 'nanoid';
 
 function App() {
-	const [personData, setPersonData] = useState<Array<Record<string, unknown>>>();
-	const { Children } = React;
+	const [personData, setPersonData] = useState<Array<Record<string, unknown>>>(); //	Create state for storing expected json data
+	const { Children } = React;	//	For displaying components of array
 
-	const fetchData = (upload:Array<Record<string, unknown>>) => {
+	const fetchData = (upload:Array<Record<string, unknown>>) => {	// Function to load data on initial render from FileInput component
 		if (typeof upload === 'object') {
 			setPersonData([...upload]);
 		}
 	};
 
-	const saveChange = useCallback((index: number, key: string, newData: string | number | boolean): void => {
+	const saveChange = useCallback((index: number, key: string, newData: string | number | boolean): void => { //	Function to save changes from input components, memoized for efficiency
 		setPersonData(prev => prev?.map((item, i) => {
 			return i === index ? { ...item, [key]: newData } : item;
 		}));
@@ -29,45 +29,40 @@ function App() {
 
 	return (
 		<>
+			{/*	conditional display of FileInput component */}
 			{!personData && <FileInput fetchData={fetchData} />}
+			{/*	display data when available */}
 			{personData && personData.map((el, index) => {
 				return (
 					<table>
 						<thead>
+							{/*	display table headers	*/}
 							<tr>
 								{
 									typeof el === 'object' &&
 									index === 0 &&
 									el !== null &&
 									Children.toArray(Object.keys(el).map((item) => {
-										const result = el[item];
-										const isString = typeof result === 'string';
-										const isDate = isString && moment(result.substring(0, 13)).isValid();
-										const islargeText = isString && result.length > 60;
-										const isBoolean = typeof result === 'boolean';
-										const isNumber = typeof result === 'number';
-										return (isDate && (<SmallTableHeader headerTitle={item} />)) ||
-											(islargeText && (<LargeTableHeader headerTitle={item} />)) ||
-											(isString && (<MediumTableHeader headerTitle={item} />)) ||
-											(isNumber && (<SmallTableHeader headerTitle={item} />)) ||
-											(isBoolean && (<SmallTableHeader headerTitle={item} />));
+										return (<MediumTableHeader headerTitle={item} />);
 									}))
 								}
 							</tr>
 						</thead>
 						<tbody>
 							<tr>
+								{/*	display contents of json object */}
 								{
 									typeof el === 'object' &&
 									el !== null &&
 									Children.toArray(Object.keys(el).map((item) => {
 										const result = el[item];
-										const isString = typeof result === 'string';
-										const isNumber = typeof result === 'number';
-										const isBoolean = typeof result === 'boolean';
-										const islargeText = isString && result.length > 60;
-										const isDate = isString && moment((el[item] as string).substring(0, 13)).isValid();
+										const isString = typeof result === 'string'; //	check for string compatibility
+										const isNumber = typeof result === 'number'; //	check for number compatibility
+										const isBoolean = typeof result === 'boolean';	//	check for boolean compatibility
+										const islargeText = isString && result.length > 60; //	check for if string is more than 60 characters
+										const isDate = isString && moment((el[item] as string).substring(0, 13)).isValid(); //	est for date compatibility
 										return (
+											//	choose display component according to compatibilty test
 											(isDate && <SmallContent content={moment((el[item] as string).substring(0, 13)).format('YYYY-MM-DD')} />) ||
 											(islargeText && <LargeContent content={result as string} />) ||
 											(isString && <SmallContent content={result as string} />) ||
@@ -77,11 +72,12 @@ function App() {
 								}
 							</tr>
 							<tr>
+								{/*	display input for edit */}
 								{
 									typeof el === 'object' &&
 									el !== null &&
 									Children.toArray(Object.keys(el).map((item) => {
-										const result = el[item];
+										const result = el[item]; 	//	compatibility test for input component
 										const isString = typeof result === 'string';
 										const isDate = isString && moment(result.substring(0, 13)).isValid();
 										const islargeText = isString && result.length > 60;
